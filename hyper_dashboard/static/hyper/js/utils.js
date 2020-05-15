2c93c2bf2b5bf30a8cb40943c4272219b07980ed
Jackson Moura <jacksonpmoura@hotmail.com>
2020-05-15 15:34:52 -0300

    Adicionado cofirm

--- hyper_dashboard/static/hyper/js/utils.js
+++ hyper_dashboard/static/hyper/js/utils.js
@@ -13,3 +13,31 @@ function resize_for_popup() {
 }
 
 resize_for_popup();
+
+(function($) {
+    $('[data-confirm]').click( function() {
+        let element = $(this)
+        event.preventDefault();
+        let text = element.attr('data-confirm-text')
+        let icon = element.attr('data-confirm-icon')
+        let title = element.attr('data-confirm-title')
+        let button_text = element.attr('data-confirm-button-text')
+
+        Swal.fire({
+              title: title,
+              text: text,
+              icon: icon,
+              showCancelButton: true,
+              confirmButtonColor: '#5398fb',
+              cancelButtonColor: '#fa6767',
+              confirmButtonText: button_text,
+              cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.value) {
+                location.href = element.attr('href')
+            }
+        })
+
+    })
+})(jQuery);
+
--- hyper_dashboard/utils/elementos.py
+++ hyper_dashboard/utils/elementos.py
@@ -1,15 +1,29 @@
 from django.utils.html import mark_safe
 
+
 def toggle(ativo, id):
     checked = 'checked' if ativo else ''
     return mark_safe('<input type="checkbox" data-on= "Sim" data-size="sm"'
                      'data-off="Não" {} id="toggle-{}" data-toggle="toggle" data-size="sm">'.format(checked, id))
 
+
 def icon(label, icone):
     return mark_safe('<i class="{}"></i> {}'.format(icone, label))
 
-def badge(nome , cor):
+
+def badge(nome, cor):
     return mark_safe('<span class="badge badge-{}-lighten p-1">{}</snpan>'.format(cor, nome))
+
 
 def avatar(imagem):
     return mark_safe('<img src = "/media/{}" alt = "user-image" height = "42" class ="avatar">'.format(imagem))
+
+
+def confirm(label='', url='', text='', icon='warning', title='Aviso', button_text='Sim'):
+    html = '<a href="{url}" data-confirm="true" ' \
+           'data-confirm-url="{url}" data-confirm-text="{text}" data-confirm-icon="warning" ' \
+           'data-confirm-button-text="{button_text}" data-confirm-title="{title}">{label}</a>'.format(
+        label=label, url=url, icon=icon, text=text, title=title, button_text=button_text
+    )
+
+    return mark_safe(html)
