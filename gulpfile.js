8749f005181f7fbaaa47eee9563dadaf440b689a
jacksonpm <jacksonpmoura@hotmail.com>
2020-07-22 15:15:40 -0300

    Modificado gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,12 @@ gulp.task('js', function () {
       .src([
           'hyper_dashboard/static/hyper/js/vendor.min.js',
           'hyper_dashboard/static/hyper/js/hyper.min.js',
-          'hyper_dashboard/static/suit/js/suit.js',
+          'hyper_dashboard/static/hyper/js/suit.js',
           'hyper_dashboard/static/hyper/js/alertas.js',
-          'hyper_dashboard/static/external/js/jquery.mask.js',
-          'hyper_dashboard/static/external/js/sweet.alert.js',
+          'hyper_dashboard/static/hyper/js/jquery.mask.js',
+          'hyper_dashboard/static/hyper/js/sweet.alert.js',
           'hyper_dashboard/static/hyper/js/date.picker.js',
-          'hyper_dashboard/static/external/js/bootstrap-select.min.js',
+          'hyper_dashboard/static/hyper/js/bootstrap-select.min.js',
           'hyper_dashboard/static/hyper/js/jquery.validate.min.js',
           'hyper_dashboard/static/hyper/js/utils.js',
       ])
@@ -23,12 +23,13 @@ gulp.task('js', function () {
 gulp.task('css', function () {
    return gulp
       .src([
+          'hyper_dashboard/static/hyper/css/suit.css',
           'hyper_dashboard/static/hyper/css/toast.css',
           'hyper_dashboard/static/hyper/css/style.css',
           'hyper_dashboard/static/hyper/css/base-admin.css',
           'hyper_dashboard/static/hyper/css/navbar.css',
-          'hyper_dashboard/static/external/css/material.icons.css',
-          'hyper_dashboard/static/external/css/bootstrap-select.min.css',
+          'hyper_dashboard/static/hyper/css/material.icons.css',
+          'hyper_dashboard/static/hyper/css/bootstrap-select.min.css',
       ])
       .pipe(concat('app.css'))
       .pipe(gulp.dest('hyper_dashboard/static/hyper/css'));
